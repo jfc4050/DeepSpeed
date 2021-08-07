13c18799f837919c96e3144bf87003261adbfb77
Justin Chiu <justchiu@amazon.com>
2021-08-07 15:57:24 -0700

    better logging of zero3 arguments

--- deepspeed/runtime/zero/stage3.py
+++ deepspeed/runtime/zero/stage3.py
@@ -569,8 +569,7 @@ class FP16_DeepSpeedZeroOptimizer_Stage3(object):
         see_memory_usage("Stage 3 initialize beginning", force=False)
 
         if dist.get_rank() == 0:
-            logger.info(f"Reduce bucket size {reduce_bucket_size}")
-            logger.info(f"Allgather bucket size {prefetch_bucket_size}")
+            logger.info(f"initialized {__class__.__name__} with args: {locals()}")
         # The fused optimizer does all the work. We need this layer for two reason:
         # 1. maintain same user API from apex.fp16_utils
         # 2. keep common stuff here in case we need to add ne552w fused optimizer later
