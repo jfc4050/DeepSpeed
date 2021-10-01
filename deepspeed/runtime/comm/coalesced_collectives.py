8a2ec421716448827f0a2a2b7e3f51496d232561
Justin Chiu <justchiu@amazon.com>
2021-10-01 13:50:50 -0700

    using reduce_scatter_base

--- deepspeed/runtime/comm/coalesced_collectives.py
+++ deepspeed/runtime/comm/coalesced_collectives.py
@@ -22,59 +22,62 @@ def reduce_scatter_coalesced(
     """simultaneously reduce-scatter a list of tensors - this can be done more
     efficiently than individual reduce scatter calls
 
-    TODO. see if PyTorch team wants a c++ verson of this for ProcessGroupNCCL,
-    also maybe a custom kernel for this could help avoid a lot of the data movement
-    below.
+    TODO. see if PyTorch team wants a c++ verson of this for ProcessGroupNCCL
     """
     this_rank = torch.distributed.get_rank(group)
     world_sz = torch.distributed.get_world_size(group)
 
-    tensor_partitions = [torch.chunk(tensor.view(-1), world_sz) for tensor in tensors]
+    partition_lst_for_each_tensor = tuple(
+        torch.chunk(tensor.view(-1),
+                    world_sz) for tensor in tensors)
+    padded_partition_sz_for_each_tensor = tuple(
+        math.ceil(t.numel() / world_sz) for t in tensors)
 
-    # interleave tensor data such that the correct reduced partitions of each tensor
+    # interleave tensor partitions such that the correct reduced partitions of each tensor
     # end up at each rank
-    tensor_partitions_for_each_rank: List[Tensor] = [None] * world_sz
-    tensor_partition_padded_sizes = [math.ceil(t.numel() / world_sz) for t in tensors]
+    tensor_partitions_lst_with_padding = []
     for rank in range(world_sz):
-        tensor_partitions_lst_with_padding = []
-
         for tensor_idx in range(len(tensors)):
             # add tensor content
-            tensor_chunk = tensor_partitions[tensor_idx][rank]
+            tensor_chunk = partition_lst_for_each_tensor[tensor_idx][rank]
             tensor_partitions_lst_with_padding.append(tensor_chunk)
 
             # add padding if necessary
-            padding_sz = tensor_partition_padded_sizes[tensor_idx] - tensor_chunk.numel()
+            padding_sz = padded_partition_sz_for_each_tensor[
+                tensor_idx] - tensor_chunk.numel()
             if padding_sz > 0:
                 tensor_partitions_lst_with_padding.append(
                     torch.empty(padding_sz,
                                 dtype=tensor_chunk.dtype,
                                 device=tensor_chunk.device))
 
-        tensor_partitions_for_each_rank[rank] = instrument_w_nvtx(
-            torch.cat)(tensor_partitions_lst_with_padding)
+    # concatenate interleaved components into a flat buffer
+    tensor_partition_flat_buffer = instrument_w_nvtx(
+        torch.cat)(tensor_partitions_lst_with_padding)
+    tensor_partition_buffer_for_each_rank: List[Tensor] = torch.chunk(
+        tensor_partition_flat_buffer,
+        world_sz)
 
     # batched reduce-scatter call
-    # TODO. try using _reduce_scatter_base
-    instrument_w_nvtx(torch.distributed.reduce_scatter)(
-        tensor_partitions_for_each_rank[this_rank],
-        tensor_partitions_for_each_rank,
+    instrument_w_nvtx(torch.distributed._reduce_scatter_base)(
+        tensor_partition_buffer_for_each_rank[this_rank],
+        tensor_partition_flat_buffer,
         group=group,
     )
 
     # post-divide
-    tensor_partitions_for_each_rank[this_rank].div_(world_sz)
+    tensor_partition_buffer_for_each_rank[this_rank].div_(world_sz)
 
     # reverse procedure of the interleaving done previously, done on the
     # result of the batched reduce-scatter
     output_lst: List[Tensor] = [None] * len(tensors)
     offset = 0
     for tensor_idx in range(len(tensors)):
-        output_lst[tensor_idx] = tensor_partitions_for_each_rank[this_rank].narrow(
+        output_lst[tensor_idx] = tensor_partition_buffer_for_each_rank[this_rank].narrow(
             0,
             offset,
-            tensor_partitions[tensor_idx][this_rank].numel())
+            partition_lst_for_each_tensor[tensor_idx][this_rank].numel())
 
-        offset += tensor_partition_padded_sizes[tensor_idx]
+        offset += padded_partition_sz_for_each_tensor[tensor_idx]
 
     return output_lst
